c404b99b24556c2f1cb908fbf0b185181b1afbe6
Joyel <Varghesejoyel71@gmail.com>
2025-03-18 13:56:01 +0530

    Fixed server export for Vercel

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ app.use(express.urlencoded({extended:false}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static('public'));
-
+// app.use(express.static('public'));
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 app.use(nocache());
 
 app.use(
