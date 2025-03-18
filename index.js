3cf7c3873693f6d7a296d836cedc890bed621b9d
Joyel <Varghesejoyel71@gmail.com>
2025-03-18 13:35:44 +0530

    last revision

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended:false}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 
 app.use(nocache());
 
